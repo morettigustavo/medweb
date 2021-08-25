1604ce7519f6624b3fa485f7c20b02d41df73a99
Gustavo Moretti <gustavomoretti165@gmail.com>
2021-08-24 21:35:25 -0300

    Primeiro Commit

---
+++ public/Firebase/firebase_config.js
@@ -0,0 +1,16 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCn_T-iMBBlsBJg-Y5KkNIrKiTwsGarO8Y",
+    authDomain: "medweb-1bfc9.firebaseapp.com",
+    databaseURL: "https://medweb-1bfc9-default-rtdb.firebaseio.com",
+    projectId: "medweb-1bfc9",
+    storageBucket: "medweb-1bfc9.appspot.com",
+    messagingSenderId: "42700337664",
+    appId: "1:42700337664:web:04c9d526e9227893be5ee5",
+    measurementId: "G-MTH4SK5EDH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
